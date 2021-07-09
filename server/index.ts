8279b6adcff339e6870c4ad3f8aeed15c30d6818
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2021-07-09 12:25:15 +0100

    change port number

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import next, { NextApiHandler } from "next"
 
 import { startServer } from "./config/startServer"
 
-const { NODE_ENV, PORT = "3000" } = process.env
+const { NODE_ENV, PORT = "5000" } = process.env
 const _PORT = parseInt(PORT, 10)
 const dev = NODE_ENV !== "production"
 
